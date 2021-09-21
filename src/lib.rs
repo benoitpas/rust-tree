1ecbab88b28e64871c989b7341f8a53694edecf3
Benoit Pasquereau <bpasquereau@gmail.com>
2021-09-21 21:11:06 +0100

    Implemented Display type class

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,22 @@
-pub enum Node<'a, Display> {
+pub enum Node<'a> {
     Leaf,
-    Branch(Display,& 'a Node<'a,Display>, &'a Node<'a,Display>)
+    Branch(&'a dyn std::fmt::Display, &'a Node<'a>, &'a Node<'a>) //Need to explain 'dyn'
 }
-
-//use std::fmt::Display;
-//impl<'a,T:Display> Node<'a,T> {
-//    fn display(&self) {
-//        match self {
-//            Node::Leaf =>
-//            println!("({},{},{})", self.)
-//
- //       }
- //   }
-//}
 
 // to finish https://doc.rust-lang.org/std/fmt/trait.Display.html
-impl<'a,T> std::fmt::Display for Node<'a,T> {
-    fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { 
-        todo!() 
-    }
-}
+impl<'a> std::fmt::Display for Node<'a> {
 
-pub fn to_string<'a,Display>(tree :&'a Node<'a,Display>) -> String {
-    match tree {
-        Node::Leaf => "",
-//            println!("({},{},{})", self.)
-        Node::Branch(value, left, right) => "(" + value.to_string() + "," + to_string(left) + "," + to_string(right) + ")"
-    }.to_string()
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { 
+        fn add_comma(input : String) -> String { 
+            if input == "" {input} else {" ,".to_string() + &input}
+        }
+    
+        let s = match self {
+            Node::Leaf => "".to_string(),
+            Node::Branch(value, left, right) => format!("({}{}{})",value.to_string(),add_comma(left.to_string()),add_comma(right.to_string()))
+        };
+        write!(f, "{}", s)
+    }
 }
 
 // For branch I have used tupple, could have used named fields (as only 2 parameters and very simple example, I prefer tupple)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use rust_tree::Node;
 
 fn main() {
-    let leaf = Node::<char>::Leaf; //mention int types including isize
-    let nd = Node::<char>::Branch('d',&leaf,&leaf); //explicit reference passing
-    let ne = Node::<char>::Branch('e',&leaf,&leaf); // I do like in Scala no having to bother with ';'
-    let nc = Node::<char>::Branch('c',&nd,&ne);
-    let nb = Node::<char>::Branch('b',&leaf,&leaf);
-    let na = Node::<char>::Branch('c',&nb,&nc);
-    println!("{}", rust_tree::to_string(&na))
+    let leaf = Node::Leaf; //mention int types including isize
+    let nd = Node::Branch(&'d',&leaf,&leaf); //explicit reference passing
+    let ne = Node::Branch(&'e',&leaf,&leaf); // I do like in Scala no having to bother with ';'
+    let nc = Node::Branch(&'c',&nd,&ne);
+    let nb = Node::Branch(&'b',&leaf,&leaf);
+    let na = Node::Branch(&'c',&nb,&nc);
+    println!("{}", na.to_string())
 
     // general remark -> very clear and helpful error messages
 

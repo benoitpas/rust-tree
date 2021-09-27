92ad7459e14480d9174c595422d91caefc69cf0a
Benoit Pasquereau <bpasquereau@gmail.com>
2021-09-27 19:52:39 +0100

    Added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,26 @@
 use std::rc::Rc;
 
+// For branch I have used tupple, could have used named fields (as only 2 parameters and very simple example, I prefer tupple)
+// Creating the recursive datastruture, first surprise is the lifetime anotations
+// Lifetime cycle https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html 'a
+
+
 #[derive(Debug)] // We could have implemented the Diplay trait instead (https://doc.rust-lang.org/std/fmt/trait.Display.html)
 pub enum Node<T> {
     Leaf,
     Branch(Rc<T>, Rc<Node<T>>, Rc<Node<T>>) 
+}
+
+// Automatically available in Haskell/Scala
+impl<T: PartialEq> PartialEq for Node<T> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self,other) {
+            (Node::Leaf ,Node::Leaf ) => true,
+            (Node::Branch(s_value, s_left, s_right),Node::Branch(o_value, o_left, o_right) )
+                => o_value == s_value && s_left == o_left && s_right == o_right,
+            (_,_) => false
+        }
+    }
 }
 
 //mention int types including isize
@@ -30,13 +47,41 @@ pub fn add_ref_count<T:std::fmt::Debug>(tree: Rc<Node<T>>) -> String {
 }
 
 
-// For branch I have used tupple, could have used named fields (as only 2 parameters and very simple example, I prefer tupple)
-// Creating the recursive datastruture, first surprise is the lifetime anotations 
-// Lifetime cycle https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html 'a
+
+
+pub fn example_tree() -> Rc<Node<char>> {
+
+    fn create_node<T>(v:T) -> Rc<Node<T>> { // Type inference is more at the scala level than Haskell/Caml
+        let leaf = Rc::new(Node::Leaf);
+        Rc::new(Node::Branch(Rc::new(v),Rc::clone(&leaf),Rc::clone(&leaf))) //explicit reference passing + clone
+    }
+
+    let nd = create_node('d'); // I do like in Scala no having to bother with ';'
+    let ne = create_node('e');
+    let nb = create_node('b');
+    let nc = Rc::new(Node::Branch(Rc::new('c'),nd,ne)); //no need for clone here as only 1 usage (let removed)
+    let na = Rc::new(Node::Branch(Rc::new('a'),nb,nc));
+    na
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_add_id() {
+        let na = example_tree();
+
+        fn create_node<T>(v:T, i:isize) -> Rc<Node<(Rc<T>,isize)>> { // Type inference is more at the scala level than Haskell/Caml
+            let leaf = Rc::new(Node::Leaf);
+            Rc::new(Node::Branch(Rc::new((Rc::new(v),i)),Rc::clone(&leaf),Rc::clone(&leaf))) //explicit reference passing + clone
+        }
+        let nd = create_node('d',2);
+        let ne = create_node('e',4);
+        let nb = create_node('b',0);
+        let nc = Rc::new(Node::Branch(Rc::new((Rc::new('c'),3)),nd,ne)); //no need for clone here as only 1 usage (let removed)
+        let na_expected = Rc::new(Node::Branch(Rc::new((Rc::new('a'),1)),nb,nc));
+
+        assert_eq!(na_expected, add_id(na,0).0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use rust_tree::Node;
 use rust_tree::add_id;
 use rust_tree::add_ref_count;
+use rust_tree::example_tree;
 use std::rc::Rc;
 
 fn main() {
-    fn create_node<T>(v:T) -> Rc<Node<T>> { // Type inference is more at the scala level than Haskell/Caml
-        let leaf = Rc::new(Node::Leaf); 
-        Rc::new(Node::Branch(Rc::new(v),Rc::clone(&leaf),Rc::clone(&leaf))) //explicit reference passing + clone
-    };
-    let nd = create_node('d'); // I do like in Scala no having to bother with ';'
-    let ne = create_node('e'); 
-    let nb = create_node('b');
-    let nc = Rc::new(Node::Branch(Rc::new('c'),nd,ne)); //no need for clone here as only 1 usage (let removed)
-    let na = Rc::new(Node::Branch(Rc::new('a'),nb,nc));
+
+    let na = example_tree();
     println!("{:?}", Rc::clone(&na));
     println!("{}", add_ref_count(Rc::clone(&na)));
 
@@ -28,4 +21,6 @@ fn main() {
     // adds ome specific to haskell (like type classes) and packages them in a language with deterministic performance (i.e. no garbage collection.)
     // Very attractive alternative to C++ which already has lambdas and where pattern matching is on its way (https://github.com/mpark/patterns ) 
     // but targetted to 2023
+    // * memory management main surprise: I did write C/C++ code long time ago but I didn't use much smart pointers at the time, mostly malloc/free and then new/delete (it was before boost becane mainstream).
+    // rust model is very well thought out and a huge improvement compared to C++.
 }
